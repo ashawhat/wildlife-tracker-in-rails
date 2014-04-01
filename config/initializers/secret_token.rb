d804d4d1f4dfb664c1f9b68d68b79cd7309ca713
Ashley Rae Lee & Brentwood Reid <student@epicodus.com>
2014-04-01 09:09:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildlifeTracker::Application.config.secret_key_base = 'f906920266da9ba5ff425dfed3d0a668aef33c0820dc4b8759ff7ad8851876331c09d20b103673086f9e67320313b33ff46da4cd77b85c815ada15699a06b59b'
