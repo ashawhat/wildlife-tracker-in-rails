956835959f4ebfa4e6e202da5897915aa230fbd8
Ashley Rae Lee & Brentwood Reid <student@epicodus.com>
2014-04-01 12:04:15 -0700

    add destroy method to species.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ WildlifeTracker::Application.routes.draw do
   match('species', { :via => :get, :to => 'species#index' })
   match('species', { :via => :get, :to => 'species#new'})
   match('species', { :via => :post, :to => 'species#create'})
+  match('species/:id', { :via => :get, :to => 'species#show' })
   match('species/:id/edit', { :via => :get, :to => 'species#edit'})
   match('species/:id', {:via => [:patch, :put], to: 'species#update'})
-  match('species/:id', { :via => :get, :to => 'species#show' })
+  match('species/:id', {:via => :delete, :to => 'species#destroy'})
 
 end
