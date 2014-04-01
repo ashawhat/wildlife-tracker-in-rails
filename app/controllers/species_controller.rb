259a324086311402555afa99fe50247b0d26c1d0
Ashley Rae Lee & Brentwood Reid <student@epicodus.com>
2014-04-01 11:10:07 -0700

    add create method to species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -5,6 +5,19 @@ class SpeciesController < ApplicationController
     render('species/index.html.erb')
   end
 
+  def new
+    @species = Species.new
+  end
+
+  def create
+    @species = Species.new(:name => params[:name])
+    if @species.save
+      render('species/success.html.erb')
+    else
+      render('species/index.html.erb')
+    end
+  end
+
   def show
     @species = Species.find(params[:id])
     render('species/show.html.erb')
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 WildlifeTracker::Application.routes.draw do
   match('species', { :via => :get, :to => 'species#index' })
+  match('species', { :via => :get, :to => 'species#new'})
+  match('species', { :via => :post, :to => 'species#create'})
   match('species/:id', { :via => :get, :to => 'species#show' })
+
 end
